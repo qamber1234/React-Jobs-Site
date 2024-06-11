fb463454342f73f0b4d03dfcdf881bbc93bcb256
Syed Qamber Ali <qamberatwork@gmail.com>
2024-06-12 03:36:52 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import React from 'react'
+import React from "react";
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
-  RouterProvider
-} from 'react-router-dom'
+  RouterProvider,
+} from "react-router-dom";
 
-import HomePage from './pages/HomePage';
-import MainLayout from './layout/MainLayout';
-import JobsPage from './pages/JobsPage';
-import NotFoundPage from './pages/NotFoundPage';
-import JobPage, { jobsLoader } from './pages/JobPage';
-import AddJobPage from './pages/AddJobPage';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/add-job' element={<AddJobPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobsLoader} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Route> 
-  )
-)
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layout/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobsLoader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
 const App = () => {
-  const msg = 'meri zindagi ha tu';
-  const poets = ['mir', 'ghalib', 'nasir', 'faraz'];
- 
-  return <RouterProvider router={router} />
-}
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
 
-export default App
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`,{
+      method: 'Delete'
+    })
+    return
+  }
+ 
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSumit={addJob} />} />
+        <Route path="/jobs/:id" element={<JobPage deleteJob={deleteJob} />} loader={jobsLoader} />
+        <Route path="/edit-job/:id" element={<EditJobPage updateJobSumit={updateJob} />} loader={jobsLoader} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+  const msg = "meri zindagi ha tu";
+  const poets = ["mir", "ghalib", "nasir", "faraz"];
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
